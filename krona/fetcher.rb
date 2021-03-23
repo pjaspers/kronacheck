3b2c4e7fa3296ed82952f2710204eb559ebba8ed
pjaspers <piet@pjaspers.com>
2021-03-23 09:58:24 +0100

    Check for a couple of days in advance, to handle gap days

--- krona/fetcher.rb
+++ krona/fetcher.rb
@@ -11,9 +11,21 @@ module Krona
     def new_data?
       last_file = Dir.glob("data/COVID19BE_CASES_MUNI_CUM_*").max
       date_part = last_file.split("_").last.split(".").first
-      next_day = Time.parse(date_part).to_date + 1
+      last_date_found = Time.parse(date_part).to_date
+
+      # Check for at least 3 days in ahead, because sometimes
+      # Sciensano adds gaps to the data. To relieve some of the burden
+      # of the hospitals for entering this data. Why they don't focus
+      # on making entering the data really easy? ,,shrug
+      [1,2,3].any? do |i|
+        puts "Checking #{last_date_found + i}..."
+        exists_remotely?(url_for(date: last_date_found + i))
+      end
+    end
+
+    def exists_remotely?(url)
       command = <<~SHELL
-      curl -s -o /dev/null -w "%{http_code}"  "#{url_for(date: next_day)}"
+      curl -s -o /dev/null -w "%{http_code}"  "#{url}"
       SHELL
       result = `#{command}`.strip
       result == "200"
